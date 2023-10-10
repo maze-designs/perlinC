93c3895721cc7ca49319a6e97250d5197534ed99
Maze <22244736+maze-designs@users.noreply.github.com>
2023-10-10 11:26:51 +0000

    Update perlin.c
    Harder, Better, Faster, Stronger

--- perlin.c
+++ perlin.c
@@ -1,115 +1,103 @@
-#include    <math.h>
-#include    <stdio.h>
-#include    <stdlib.h>
-#include    <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-float noise(int x, int y)
-{
-  int n;
+#define COLS       6
+#define ROWS       6
+#define GAIN       2.6f
+#define OCTAVES    2
+#define HGRID      35
+#define MULT       5
+#define LACUNARITY 2
 
-  n = x + y * 57;
-  n = pow((n << 13), n);
-  return (1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
+#ifndef M_PI
+#define M_PI 3.141592653589793f
+#endif
+
+static float noise(const int x, const int y) {
+    int n;
+
+    n = x + y * 57;
+    n = pow(n << 13, n);
+
+    return 1.0f - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) /
+                      1073741824.0f;
 }
 
-float interpolate(float a, float b, float x)
-{
-  float pi_mod;
-  float f_unk;
-
-  pi_mod = x * 3.1415927;
-  f_unk = (1 - cos(pi_mod)) * 0.5;
-  return (a * (1 - f_unk) + b * x);
+static float interpolate(const float a, const float b, const float x) {
+    return a * (1 - (1 - cos(x * M_PI)) * 0.5f) + b * x;
 }
 
-float smooth_noise(int x, int y)
-{
-  float corners;
-  float center;
-  float sides;
+static float smooth_noise(const int x, const int y) {
+    float corners, center, sides;
 
-  corners = (noise(x - 1, y - 1) + noise(x + 1, y - 1) +
-   noise(x - 1, x + 1) + noise(x + 1, y + 1)) / 16;
-  sides = (noise(x - 1, y) + noise(x + 1, y) + noise(x, y - 1) +
-  noise(x, y + 1)) / 8;
-  center = noise(x, y) / 4;
-  return (corners + sides + center);
+    corners = (noise(x - 1, y - 1) + noise(x + 1, y - 1) + noise(x - 1, x + 1) +
+               noise(x + 1, y + 1)) /
+              16;
+    sides = (noise(x - 1, y) + noise(x + 1, y) + noise(x, y - 1) +
+             noise(x, y + 1)) /
+            8;
+    center = noise(x, y) / 4;
+
+    return corners + sides + center;
 }
 
-float noise_handler(float x, float y)
-{
-  int int_val[2];
-  float frac_val[2];
-  float value[4];
-  float res[2];
+static float noise_handler(const float x, const float y) {
+    int int_val[2];
+    float frac_val[2], value[4], res[2];
 
-  int_val[0] = (int)x;
-  int_val[1] = (int)y;
-  frac_val[0] = x - int_val[0];
-  frac_val[1] = y - int_val[1];
-  value[0] = smooth_noise(int_val[0], int_val[1]);
-  value[1] = smooth_noise(int_val[0] + 1, int_val[1]);
-  value[2] = smooth_noise(int_val[0], int_val[1] + 1);
-  value[3] = smooth_noise(int_val[0] + 1, int_val[1] + 1);
-  res[0] = interpolate(value[0], value[1], frac_val[0]);
-  res[1] = interpolate(value[2], value[3], frac_val[0]);
-  return (interpolate(res[0], res[1], frac_val[1]));
+    int_val[0] = x;
+    int_val[1] = y;
+
+    frac_val[0] = x - int_val[0];
+    frac_val[1] = y - int_val[1];
+
+    value[0] = smooth_noise(int_val[0], int_val[1]);
+    value[1] = smooth_noise(int_val[0] + 1, int_val[1]);
+    value[2] = smooth_noise(int_val[0], int_val[1] + 1);
+    value[3] = smooth_noise(int_val[0] + 1, int_val[1] + 1);
+
+    res[0] = interpolate(value[0], value[1], frac_val[0]);
+    res[1] = interpolate(value[2], value[3], frac_val[0]);
+
+    return interpolate(res[0], res[1], frac_val[1]);
 }
 
-float perlin_two(float x, float y, float gain, int octaves, int hgrid) {
-    int i;
-    float total = 0.0f;
-    float frequency = 1.0f/(float)hgrid;
-    float amplitude = gain;
-    float lacunarity = 2.0;
+static float perlin_two(const float x, const float y) {
+    unsigned idx;
+    float total = 0, frequency = 1.0f / HGRID, amplitude = GAIN;
 
-    for (i = 0; i < octaves; ++i)
-    {
-  total += noise_handler((float)x * frequency, (float)y * frequency) * amplitude;
-  frequency *= lacunarity;
-  amplitude *= gain;
+    for (idx = 0; idx < OCTAVES; ++idx) {
+        total += noise_handler(x * frequency, y * frequency) * amplitude;
+        frequency *= LACUNARITY;
+        amplitude *= GAIN;
     }
 
-    return (total);
+    return total;
 }
 
-int * normalizeArray(float *array[], size_t arraySize, int maxNum) {
-  int *retArray = malloc((arraySize/sizeof(array[0]))*sizeof(int));
-  retArray[0] = (int)array[0];
-  return retArray;
-}
+int main(void) {
+    int row;
+    unsigned seed, idx;
+    float noise_arr[COLS * ROWS];
 
-void main() {
-  srand(time(NULL));
-float seed = rand() % 512;
-printf("%f\n", seed);
-static int cols = 6;
-static int rows = 6;
-static float gain = 2.6f;
+    srand(time(NULL));
+    seed = rand() % 512;
 
-static int octaves = 2;
-static int hgrid = 35;
+    printf("%u\n", seed);
 
-static int multiplier = 5;
-float noiseArr [cols*rows];
-//printf("%f\n", perlin_two(1.0,2.0,1.0,1,1));
-
-for (int i = 0; i < cols * rows; i++) {
- int row = (i-i%cols)/cols;
- int col = i-(cols*row);
- //printf("%i %i\n", col, row);
- float res = multiplier * perlin_two(seed+col, row, gain, octaves, hgrid);
- noiseArr[i] = 0 - res;
-}
-for (int i = 0; i < sizeof(noiseArr) / sizeof(noiseArr[0]); i++) {
-  printf("%f ", noiseArr[i]);
-    if (i == cols * rows - 1 || (i+1)%cols == 0 && i != 0) {
-  printf("\n");
+    for (idx = 0; idx < COLS * ROWS; idx++) {
+        row            = (idx - idx % COLS) / COLS;
+        noise_arr[idx] = -(MULT * perlin_two(seed + (idx - (COLS * row)), row));
     }
+
+    for (idx = 0; idx < sizeof(noise_arr) / sizeof(noise_arr[0]); idx++) {
+        printf("%f ", noise_arr[idx]);
+
+        if (idx == COLS * ROWS - 1 || ((idx + 1) % COLS == 0 && idx != 0))
+            putc('\n', stdout);
+    }
+
+    return 0;
 }
-
-printf("%i", normalizeArray(&noiseArr, sizeof(noiseArr), 1));
-
-}
-
-
